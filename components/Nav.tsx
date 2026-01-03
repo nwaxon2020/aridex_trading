78e6f2d7e528d6684f500f7ac659a146c4263076
nwaxon2020 <princenwachukwu308@yahoo.com>
2026-01-03 11:17:13 +0100

    blod and chat adjusted

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,14 +1,15 @@
+import Link from "next/link";
 export default function Nav() {
     return (
         <div className="bg-blue-300">
             <div className="flex justify-between items-center z-80 px-8 py-3 bg-[rgba(0,0,0,0.7)]">
-                <div className="w-14 h-14 md:w-20 md:h-20 rounded-full border-2 border-white overflow-hidden">
+                <Link href={"/"} className="w-14 h-14 md:w-20 md:h-20 rounded-full border-2 border-white overflow-hidden">
                     <img
                         src={"/abidex_logo.jpeg"}
                         alt="Logo"
                         className="w-full h-full object-cover"
                     />
-                </div>
+                </Link>
 
                 <div className="flex flex-col gap-1">
                     <h1 className="text-right md:text-left text-2xl md:text-3xl font-extrabold tracking-tight bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 bg-clip-text text-transparent">
