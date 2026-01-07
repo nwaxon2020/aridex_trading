6103390a67e1143ede976e26b1ad35e339e596f9
nwaxon2020 <princenwachukwu308@yahoo.com>
2026-01-07 17:47:54 +0100

    git 3

--- lib/firebaseconfig.ts
+++ lib/firebaseconfig.ts
@@ -1,3 +1,27 @@
+// // lib/firebaseconfig.ts
+// import { initializeApp, getApps } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
+// import { getStorage } from "firebase/storage";
+
+// const firebaseConfig = {
+//   NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyARpzCYjZrxuYklP1PxU2CDGBo3S36s3oc",
+//   NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "abitex-119cc.firebaseapp.com",
+//   NEXT_PUBLIC_FIREBASE_PROJECT_ID: "abitex-119cc",
+//   NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "abitex-119cc.firebasestorage.app",
+//   NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "223546128782",
+//   NEXT_PUBLIC_FIREBASE_APP_ID: "1:223546128782:web:d2829920a272cf38ae0cdf",
+//   NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: "G-5LYYX37YEX",
+
+// };
+
+// const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+// const db = getFirestore(app);
+// const auth = getAuth(app);
+// const storage = getStorage(app);
+
+// export { db, auth, storage };
+
 // lib/firebaseconfig.ts
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -5,14 +29,16 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyARpzCYjZrxuYklP1PxU2CDGBo3S36s3oc",
+  authDomain: "abitex-119cc.firebaseapp.com",
+  projectId: "abitex-119cc",
+  storageBucket: "abitex-119cc.firebasestorage.app",
+  messagingSenderId: "223546128782",
+  appId: "1:223546128782:web:d2829920a272cf38ae0cdf",
+  measurementId: "G-5LYYX37YEX"
 };
 
+// Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 const db = getFirestore(app);
 const auth = getAuth(app);
